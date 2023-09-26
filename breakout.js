11c4217a221ed9c127955d9183876aa3768d189d
Isaac2109 <lrsiazevedo2018@gmail.com>
2023-09-26 17:03:12 -0300

    Update breakout.js
    Comecei a fazer a troca de direções da bola para ela não sair da tela.

--- breakout.js
+++ breakout.js
@@ -4,6 +4,8 @@ var bar = window.document.getElementById('barra')
 var ball = document.getElementById('bola')
 var board = window.document.getElementById('quadro')
 var width_board = window.document.getElementById('quadro').clientWidth
+var height_board = window.document.getElementById('quadro').clientHeight
+console.log(height_board)
 window.onload = clock
 
 // movimento da barra
@@ -28,36 +30,44 @@ var UP_RIGHT = 0
 var DOWN_RIGHT = 1
 var UP_LEFT = 2
 var DOWN_LEFT = 3
-var direction = DOWN_LEFT
+var ball_direction = UP_RIGHT
 var margim_top = 400
 var margim_left = 200
 
 function clock() {
-    if (direction == UP_RIGHT) {
+    if (ball_direction == UP_RIGHT) {
         margim_top -= 5
         margim_left += 5
         ball.style.marginTop = `${margim_top}px`
         ball.style.marginLeft = `${margim_left}px`
     }
-    if (direction == DOWN_RIGHT){
+    if (ball_direction == DOWN_RIGHT){
         margim_top += 5
         margim_left += 5
         ball.style.marginTop = `${margim_top}px`
         ball.style.marginLeft = `${margim_left}px`
     }
-    if (direction == UP_LEFT) {
+    if (ball_direction == UP_LEFT) {
         margim_top -= 5
         margim_left -= 5
         ball.style.marginTop = `${margim_top}px`
         ball.style.marginLeft = `${margim_left}px`
     }
-    if (direction == DOWN_LEFT) {
+    if (ball_direction == DOWN_LEFT) {
         margim_top += 5
         margim_left -= 5
         ball.style.marginTop = `${margim_top}px`
         ball.style.marginLeft = `${margim_left}px`
     }
 
+    //tabela da bola
+
+    if (margim_top < 5 && ball_direction == UP_RIGHT) {
+        ball_direction = DOWN_RIGHT
+    }
+    if (margim_top < 5 && ball_direction == UP_LEFT) {
+        ball_direction
+    }
 
 
     temporizador = setTimeout(clock, 30);
