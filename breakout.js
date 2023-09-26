da39008711fc412bcb4f68c707c0cda11436de9e
Isaac2109 <lrsiazevedo2018@gmail.com>
2023-09-26 14:52:48 -0300

    Update Breakout-for-web
    Neste update terminei o movimento da barra.

--- breakout.js
+++ breakout.js
@@ -1,4 +1,18 @@
-function pegaTecla(){
-    var tecla = event.keyCode;
-    window.alert(tecla);
-}
+var margim_left_bar = 170
+var keycode = ''
+var bar = window.document.getElementById('barra')
+var ball = document.getElementById('bola')
+var board = window.document.getElementById('quadro')
+var width_board = window.document.getElementById('quadro').clientWidth
+document.body.addEventListener('keydown', function (event) {
+    keycode = event.keyCode
+    //window.alert(`Key: ${key}, Code ${code}`);
+    if (keycode == 39 && (margim_left_bar + 100) < width_board) {
+        margim_left_bar += 10
+        bar.style.marginLeft = `${margim_left_bar}px`
+    }
+    if (keycode == 37 && margim_left_bar > 0) {
+        margim_left_bar -= 10
+        bar.style.marginLeft = `${margim_left_bar}px`
+    }
+})
